b0a072eefd66d85eca80e659ab83536a28ff389f
Ievgen Bardadym <ievgen.bardadym@gmail.com>
2024-09-19 02:08:05 +0300

    feat(UnitTests): Add SumCalculator class with method sum to calculate integers from 1 to given number

---
+++ UnitTests/src/main/java/org/example/SumCalculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class SumCalculator {
+    public int sum(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must be greater than 0");
+        }
+        int result = 0;
+        for (int i = 1; i <= n; i++) {
+            result += i;
+        }
+        return result;
+    }
+}
